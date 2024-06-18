9f733567bbb57df5076c987feb8a884524373d17
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-06-18 23:34:47 +0530

    Update mongoose.js

--- Authentication/connection/mongoose.js
+++ Authentication/connection/mongoose.js
@@ -1,3 +1,3 @@
 const PORT = 3000;
-const mongoDBURL = 'mongodb+srv://katiyarayush02:iHoqyYkBzBBNbMR6@aerick.1833ful.mongodb.net/'
+const mongoDBURL = 'mongodb+srv://username:<password>.mongodb.net/'
 module.exports = { PORT, mongoDBURL };
