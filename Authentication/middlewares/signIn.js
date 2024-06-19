2c7b0d399f2008abaed00bdd859d212a9ea13b9a
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-06-19 10:01:01 +0530

    Update signIn.js

--- Authentication/middlewares/signIn.js
+++ Authentication/middlewares/signIn.js
@@ -7,12 +7,12 @@ async function signIn(req, res) {
     try {
         const user = await Users.findOne({ email });
         if (!user) {
-            return res.status(404).json({ msg: "User not found" });
+            return res.status(404).json({ msg: "User not found. Please check the provided credentials and try again." });
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
-            return res.status(401).json({ msg: "Wrong password" });
+            return res.status(401).json({ msg: "Wrong password. Please check your password and try again." });
         }
 
         res.json({ msg: "Signing in successfully" });
