7d8347e720ec497b69e00135d8218ff523bda384
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-06-19 09:59:10 +0530

    Update zod.js

--- Authentication/zod.js
+++ Authentication/zod.js
@@ -1,12 +1,12 @@
 const { z } = require('zod');
 
 const User = z.object({
-    name: z.string().min(1, "Name is required"),
-    email: z.string().email("Invalid email address"),
+    name: z.string().min(1, "Name is required."),
+    email: z.string().email("Invalid email address."),
     password: z.string()
-        .min(8, "Password must be at least 8 characters long")
+        .min(8, "Password must be at least 8 characters long.")
         .refine(value => /[A-Z]/.test(value) && /\d/.test(value), {
-            message: "Password must contain at least one uppercase letter and one number"
+            message: "Password must contain at least one uppercase letter and one number."
         })
 });
 
